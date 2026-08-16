86f7dc3b62a9d4d9c86aecc36b87cdbd0a7ea71a
Mohd Mazin <mohdmazin71@gmail.com>
2026-08-17 01:34:35 +0530

    Time: 59 ms (37.78%), Space: 30.5 MB (88.15%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        c0 = c1 = c2 = 0
+
+        for x in stones:
+            if x % 3 == 0:
+                c0 += 1
+            elif x % 3 == 1:
+                c1 += 1
+            else:
+                c2 += 1
+
+        if c0 % 2 == 0:
+            return c1 > 0 and c2 > 0
+
+        return abs(c1 - c2) > 2

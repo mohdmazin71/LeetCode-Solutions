6c23d746c1a4ff0cd95b8910d028ae8c9f4b1d87
Mohd Mazin <mohdmazin71@gmail.com>
2026-08-28 00:08:20 +0530

    Time: 0 ms (100%), Space: 48.3 MB (92.71%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        
+        if (start < 0 || start >= arr.length || arr[start] < 0) {
+            return false;
+        }
+        
+        
+        if (arr[start] == 0) {
+            return true;
+        }
+        
+       
+        int jump = arr[start];
+        
+      
+        arr[start] = -arr[start];
+    
+        boolean canReachTarget = canReach(arr, start + jump) || canReach(arr, start - jump);
+        
+       
+        return canReachTarget;
+    }
+}
